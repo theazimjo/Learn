5446cdf5b93be1a9d12fc563d9dd57fc0612e8b4
Azimjon <azimjonxolmirzayev30@gmail.com>
2026-03-14 16:28:46 +0500

    Mahsulotlar kartochkasi dizayni yangilandi va Coil kutubxonasi qo'shildi.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,6 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
--- app/src/main/java/abs/uits/learn/ui/screens/home/Home.kt
+++ app/src/main/java/abs/uits/learn/ui/screens/home/Home.kt
@@ -1,14 +1,29 @@
 package abs.uits.learn.ui.screens.home
 
+import abs.uits.learn.R
 import abs.uits.learn.domain.model.Product
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,8 +33,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 
 
 @Composable
@@ -36,7 +57,9 @@ fun Home(modifier: Modifier = Modifier, viewModel: HomeViewModel) {
         modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        LazyRow() {
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
             items(products) { product ->
                 ProductItem(modifier = Modifier, product)
             }
@@ -47,11 +70,59 @@ fun Home(modifier: Modifier = Modifier, viewModel: HomeViewModel) {
 
 @Composable
 fun ProductItem(modifier: Modifier = Modifier, products: Product) {
-    Column(modifier
-        .width(60.dp)
-        .height(60.dp)) {
-        Text(products.title)
-        Text(products.description)
+    Card(
+        modifier
+            .width(170.dp)
+            .height(260.dp)
+            .clip(RoundedCornerShape(15.dp)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(
+            modifier
+                .fillMaxSize()
+                .padding(15.dp),
+
+            ) {
+            AsyncImage(
+                model = products.thumbnail,
+                contentDescription = "Rasm",
+                modifier.height(140.dp),
+                contentScale = ContentScale.Crop,
+            )
+            Spacer(modifier.height(5.dp))
+            Text(
+                text = products.title,
+                fontSize = 13.sp,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1
+            )
+            Spacer(modifier.height(5.dp))
+            Row(
+                modifier.fillMaxWidth(),
+            ) {
+                Text(text = "${products.weight}", fontSize = 9.sp)
+                Text(text = products.brand, fontSize = 9.sp)
+            }
+            Spacer(modifier.height(8.dp))
+
+            Row(
+                modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "${products.price}")
+                Button(
+                    onClick = {},
+                    modifier
+                        .width(60.dp)
+                        .height(60.dp),
+                    shape = RoundedCornerShape(17.dp),
+                ) {
+                    Image(painter = painterResource(R.drawable.plus), contentDescription = "")
+                }
+            }
+
+        }
 
     }
 }
